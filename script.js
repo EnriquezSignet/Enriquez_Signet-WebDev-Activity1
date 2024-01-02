fe1c1c917fb64ef46531594c714de38435c9d1fb
EnriquezSignet <144646915+EnriquezSignet@users.noreply.github.com>
2024-01-03 05:32:43 +0800

    commit changes

--- script.js
+++ script.js
@@ -1,15 +1,128 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+/* ========== TOGGLE NAVBAR ========== */
+
+const iconToggle = document.querySelector(".toggle_icon");
+const navbarMenu = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll(".menu_link");
+const iconClose = document.querySelector(".close_icon");
+
+iconToggle.addEventListener("click", () => {
+  navbarMenu.classList.toggle("active");
+});
+
+iconClose.addEventListener("click", () => {
+  navbarMenu.classList.remove("active");
+});
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener("click", () => {
+    navbarMenu.classList.remove("active");
+  });
+});
+
+/* ========== BACKGROUND HEADER ========== */
+
+function scrollHeader() {
+  const header = document.getElementById("header");
+  this.scrollY >= 20
+    ? header.classList.add("active")
+    : header.classList.remove("active");
 }
 
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
+window.addEventListener("scroll", scrollHeader);
+
+/* ========== HOME TYPE ANIMATION ========== */
+
+const typed = document.querySelector(".typed");
+if (typed) {
+  let typed_strings = typed.getAttribute("data-typed-items");
+  typed_strings = typed_strings.split(",");
+  new Typed(".typed", {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
   });
+}
+
+/* ========== ACTIVE SECTION ========== */
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 50;
+
+    let sectionId = section.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".menu a[href *=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".menu a[href *=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+
+/* ========== EXPERTISE ========== */
+
+const pages = document.querySelectorAll(".page");
+
+const expertise = document.querySelector(".expertise");
+
+function expertiseActive() {
+  const scrollY = window.pageYOffset;
+
+  pages.forEach((page) => {
+    const sectionHeight = page.offsetHeight;
+    const sectionTop = page.offsetTop - 120;
+
+    let sectionId = page.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".expertise_tabs a[href *=" + sectionId + "]")
+        .classList.add("current");
+    } else {
+      document
+        .querySelector(".expertise_tabs a[href *=" + sectionId + "]")
+        .classList.remove("current");
+    }
+  });
+}
+
+window.addEventListener("scroll", expertiseActive);
+
+/* ========== PORTFOLIO ========== */
+
+let filterItems = document.querySelectorAll(".portfolio_filters li");
+
+function activePortfolio() {
+  filterItems.forEach((el) => {
+    el.classList.remove("filter-active");
+    this.classList.add("filter-active");
+  });
+}
+
+filterItems.forEach((el) => {
+  el.addEventListener("click", activePortfolio);
+});
+
+/* ========== MIXITUP ========== */
+
+let mixerPortfolio = mixitup(".portfolio_wrap-container", {
+  selectors: {
+    target: ".portfolio_item",
+  },
+  animation: {
+    duration: 300,
+  },
 });
